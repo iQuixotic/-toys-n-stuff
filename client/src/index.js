391efe30de655a89d176ab3a3436eeaec539e6b4
Trey <treyw1850@gmail.com>
2018-07-06 14:51:51 -0400

    added a normalize.css file. more styling to come

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './master/normalize.css';
 import "./master/main.css";
 import App from "./master/App";
 import registerServiceWorker from "./master/registerServiceWorker";
